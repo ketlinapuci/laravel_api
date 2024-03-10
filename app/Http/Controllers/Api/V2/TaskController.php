e6ee460e4a2b438605c5776c1188aaf6c06c8399
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-03-10 22:06:09 +0100

    authorize other endpoints

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -10,6 +10,11 @@ use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+
+    }
     /**
      * Display a listing of the resource.
      */
@@ -37,7 +42,6 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        $this->authorize('view', $task);
         return TaskResource::make($task);
     }
 
--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -14,7 +14,7 @@ class TaskPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -25,20 +25,13 @@ class TaskPolicy
         return $user->id == $task->user_id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
 
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Task $task): bool
     {
-        //
+        return $user->id == $task->user_id;
     }
 
     /**
@@ -46,22 +39,8 @@ class TaskPolicy
      */
     public function delete(User $user, Task $task): bool
     {
-        //
+        return $user->id == $task->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Task $task): bool
-    {
-        //
-    }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Task $task): bool
-    {
-        //
-    }
 }
